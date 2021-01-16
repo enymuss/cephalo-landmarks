f34290b4363f86a4ae1299990a0ccd40d35fa328
Richard Szczerba <richardszczerba@gmail.com>
2021-01-16 18:02:47 +0100

    add cephalo and cephalo_landmarks

--- backend/app/app/db/base.py
+++ backend/app/app/db/base.py
@@ -3,3 +3,5 @@
 from app.db.base_class import Base  # noqa
 from app.models.item import Item  # noqa
 from app.models.user import User  # noqa
+from app.models.cephalo import Cephalo # noqa
+from app.models.cephalo_landmark import Cephalo_Landmark # noqa
