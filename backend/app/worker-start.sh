ca9d8f75d3e2f6cfa2e6275ebabbaf6a0b6582eb
Richard Szczerba <richardszczerba@gmail.com>
2021-02-28 10:43:44 +0100

    update .gitignore

--- backend/app/worker-start.sh
+++ backend/app/worker-start.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env bash
 set -e
 
+aws s3 sync s3://cephalo/images-512/ app/nn_models/cephalo/inputs
+aws s3 sync s3://cephalo/models/ app/nn_models/cephalo/models
+
 python /app/app/celeryworker_pre_start.py
 
 celery worker -A app.worker -l info -Q main-queue -c 1
